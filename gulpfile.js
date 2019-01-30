75db39cb6ba4100c7eabb62168311a9dbbac1f70
Unknown <gkbarta@yahoo.com>
2019-01-30 17:21:36 +0100

    Refactor code

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+/* Template:
+gulp.task('task_name', function(){
+    // do something..
+});
+
+*/
+// Pipe is chain something together
+/*
+Returns scss file pipe it through sass and
+*/
+
+gulp.task('default', async function () {
+    console.log('Hello Gulp!')
+});
+
+gulp.task('sass', function(){
+    return gulp.src('scss/styles.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css'))
+});
