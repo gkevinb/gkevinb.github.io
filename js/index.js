7a4a53ea089420dff470af320ce310c8fbb664de
Gabor Kevin Barta <gkbarta@yahoo.com>
2019-01-31 22:35:02 +0100

    Experiment with Vue

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
+"use strict"
+
 var app = new Vue({
 	el: "#app",
     data: {
-        message: 'hello you!!'
+        message: "hello you!! " + foo("man"),
+		message2: "<strong>YOU</strong>",
+		message3: "default"
     }
 })
+
+function foo(bar){
+	bar += " !!!!!!!!!";
+	return bar;
+}
