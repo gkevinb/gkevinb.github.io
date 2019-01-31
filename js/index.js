eaa7e6a4ce88a243ac32bf3cf2ef3d834bbc09e7
Gabor Kevin Barta <gkbarta@yahoo.com>
2019-02-01 00:53:21 +0100

    Add feature to dynamically create checkbox matrix

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ var app = new Vue({
 	el: "#app",
     data: {
 		seen: false,
+		row: "5",
+		column: "2",
         message: "hello you!! " + foo("man"),
 		message2: "<strong>YOU</strong>",
 		message3: "default",
@@ -12,7 +14,16 @@ var app = new Vue({
 			{ text: "Take shower" },
 			{ text: "Go to work" }
 		]
-    }
+    },
+	methods: {
+		stringToNum: function(string){
+			return parseInt(string, 10);
+		},
+		matrixId: function(i, j){
+			return "_" + i.toString() + "x" + j.toString() + "_";
+		}
+	}
+
 })
 
 function foo(bar){
