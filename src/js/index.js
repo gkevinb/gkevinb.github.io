54150c25203a12db0694cc52cada58cd92af149d
Gabor Kevin Barta <gkbarta@yahoo.com>
2019-07-13 13:35:39 +0200

    :sparkles: Close #95 Implement give credit feature in footer

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ var app = new Vue({
         currentTab: "Home",
         tabs: [
             {
-                name: "Home"
+                name: "Home",
             },
             {
                 name: "About",
@@ -13,7 +13,17 @@ var app = new Vue({
                         name: "Experience"
                     },
                     {
-                        name: "Business Card"
+                        name: "Business Card",
+                        sources: [
+                            {
+                                name: "3D Card Flip Animation",
+                                link: "https://3dtransforms.desandro.com/card-flip"
+                            },
+                            {
+                                name: "Font Awesome Free",
+                                link: "https://fontawesome.com/"
+                            }
+                        ]
                     }
                 ]
             },
@@ -21,10 +31,30 @@ var app = new Vue({
                 name: "Projects",
                 subtabs: [
                     {
-                        name: "Q-Learning"
+                        name: "Q-Learning",
+                        sources: [
+                            {
+                                name: "Robot 2 icon by Icons8",
+                                link: "https://icons8.com/icon/22858/robot-2"
+                            },
+                            {
+                                name: "Trophy icon by Icons8",
+                                link: "https://icons8.com/icon/16951/trophy"
+                            }
+                        ]
                     },
                     {
-                        name: "Chinese Flashcards"
+                        name: "Chinese Flashcards",
+                        sources: [
+                            {
+                                name: "3D Card Flip Animation",
+                                link: "https://3dtransforms.desandro.com/card-flip"
+                            },
+                            {
+                                name: "Hanzi Chinese Characters Writer",
+                                link: "https://chanind.github.io/hanzi-writer/"
+                            }
+                        ]
                     }
                 ]
             },
@@ -62,6 +92,33 @@ var app = new Vue({
                 hamburger.classList.toggle("is-active");
                 document.getElementById("nav-burger-meat-js").classList.toggle("nav-burger-meat__X");
             }
+        },
+        getSources: function() {
+            var sources = null;
+            // console.log(this.tabs);
+            for(var tab of this.tabs){
+                // console.log(tab.name);
+                // console.log(this.currentTab);
+                if(tab.name == this.currentTab){
+                    if(tab.sources != null){
+                        sources = tab.sources;
+                    }
+                }
+                if(tab.subtabs != null){
+                    for(var subtab of tab.subtabs){
+                        console.log(subtab.name);
+                        console.log(this.currentTab);
+                        if(subtab.name == this.currentTab){
+                            console.log("X")
+                            if(subtab.sources != null){
+                                console.log("XW")
+                                sources = subtab.sources;
+                            }
+                        }
+                    }
+                }
+            }
+            return sources;
         }
     }
 });
