b63fddad41bfaf1a390e375ff649cae1a26b61aa
Gabor Kevin Barta <gkbarta@yahoo.com>
2019-09-04 12:19:58 +0200

    :bug: Fix bug with name of Github projects switched around

--- src/js/github-projects-comp.js
+++ src/js/github-projects-comp.js
@@ -5,12 +5,14 @@ Vue.component("github-projects-tab", {
                 <h1>Github Projects</h1>
                 <p>Here are some of my Github Projects which also have a separate Github Pages website.</p>
                 <p>Note some of them might be incomplete or just really simple projects.</p>
-                <h2>vue-app</h2>
+                
+                <h2>cloudera-github</h2>
                 <h5>Repo</h5>
                 <a href="https://github.com/gkevinb/cloudera-github">https://github.com/gkevinb/cloudera-github</a>
                 <h5>Website</h5>
                 <a href="https://gkevinb.github.io/cloudera-github/cloudera-github/">https://gkevinb.github.io/cloudera-github/cloudera-github/</a>
-                <h2>cloudera-github</h2>
+                
+                <h2>vue-app</h2>
                 <h5>Repo</h5>
                 <a href="https://github.com/gkevinb/vue-app">https://github.com/gkevinb/vue-app</a>
                 <h5>Website</h5>
