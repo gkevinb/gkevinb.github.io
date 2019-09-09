0a66794bc9d9bb9a1eb2082d1a199bf886be4775
Gabor Kevin Barta <gkbarta@yahoo.com>
2019-09-09 19:13:07 +0200

    :construction: Preliminary work on Github Projects section layout

--- src/components/GithubProjects/GithubProjects.vue
+++ src/components/GithubProjects/GithubProjects.vue
@@ -4,20 +4,6 @@
     <p>Here are some of my Github Projects which also have a separate Github Pages website.</p>
     <p>Note some of them might be incomplete or just really simple projects.</p>
 
-    <!-- <h2>cloudera-github</h2>
-    <h5>Repo</h5>
-    <a href="https://github.com/gkevinb/cloudera-github">https://github.com/gkevinb/cloudera-github</a>
-    <h5>Website</h5>
-    <a
-      href="https://gkevinb.github.io/cloudera-github/cloudera-github/"
-    >https://gkevinb.github.io/cloudera-github/cloudera-github/</a>
-
-    <h2>vue-app</h2>
-    <h5>Repo</h5>
-    <a href="https://github.com/gkevinb/vue-app">https://github.com/gkevinb/vue-app</a>
-    <h5>Website</h5>
-    <a href="https://gkevinb.github.io/vue-app/">https://gkevinb.github.io/vue-app/</a> -->
-
     <h2>Repos</h2>
     <div v-for="repo in repos" :key="repo.id" class="repo-container">
       <h4>{{ repo.name }}</h4>
@@ -48,4 +34,10 @@ export default {
 </script>
 
 <style lang="scss">
+
+.repo-container{
+  display: flex;
+  flex-direction: column;
+}
+
 </style>
