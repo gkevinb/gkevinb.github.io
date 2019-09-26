c2bc5626ff3d393a6061378b4ca6d7d176d15894
Gabor Kevin Barta <gkbarta@yahoo.com>
2019-09-26 12:33:30 +0200

    :sparkles: Close #134 Add collapse button for custom scripts

--- src/components/CustomScripts/CustomScripts.js
+++ src/components/CustomScripts/CustomScripts.js
@@ -16,29 +16,27 @@ export default {
             readMe: null,
             commands: {},
             show: [],
+            reRenderKey: 0
         };
     },
     created() {
         hljs.registerLanguage('bash', bash);
         hljs.registerLanguage('python', python);
 
+        let showIndex = 0
+
         for (let i = 0; i < this.input.length; i++) {
             let file = this.input[i]
 
-            if (file.type == 'file') {
-                /* Perhaps make regex check better */
-                let pattern = /.+\..+/g;
-                
-                if (!pattern.test(file.name)) {
-                    this.commands[file.name] = {}
-                    this.commands[file.name]['id'] = i;
-                    this.commands[file.name]['name'] = file.name;
-                    this.show.push(false)
-                    this.apiCall(file.name, file.download_url)
-                }
-            }
             if (file.name == 'README.md') {
                 this.apiCallReadMe(file.download_url)
+            } else {
+                this.commands[file.name] = {}
+                this.commands[file.name]['id'] = showIndex
+                this.commands[file.name]['name'] = file.name;
+                this.show.push(false)
+                this.apiCall(file.name, file.download_url)
+                showIndex = showIndex + 1
             }
         }
     },
@@ -64,10 +62,10 @@ export default {
 
                     this.commands[name]['algorithm'] = algorithm
 
-                    if(pythonPattern.test(algorithm)){
+                    if (pythonPattern.test(algorithm)) {
                         this.commands[name]['language'] = 'python'
                     }
-                    if(bashPattern.test(algorithm)){
+                    if (bashPattern.test(algorithm)) {
                         this.commands[name]['language'] = 'bash'
                     }
                 });
@@ -79,16 +77,21 @@ export default {
                     this.readMe = response.data;
                 });
         },
-        classStyling: function(file){
-            if(this.commands[file]['language'] == 'python'){
+        classStyling: function (file) {
+            if (this.commands[file]['language'] == 'python') {
                 return "hjls python"
             }
-            if(this.commands[file]['language'] == 'bash'){
+            if (this.commands[file]['language'] == 'bash') {
                 return "hjls bash"
-            }
-            else{
+            } else {
                 return "hjls"
             }
+        },
+        collapseAll: function () {
+            for (let i = 0; i < this.show.length; i++) {
+                this.show[i] = false
+            }
+            this.reRenderKey += 1
         }
     }
 };
