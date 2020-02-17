bece73077f45b948126bb8ebcd5a6699d1da56aa
Gabor Kevin Barta <gkevinba@gmail.com>
2020-02-18 00:24:22 +0100

    :chart_with_upwards_trend: Inject Google Analytics tracking code using npm tool

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App/App.vue'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import VueAnalytics from 'vue-analytics'
 
+Vue.use(VueAnalytics, {
+  id: 'UA-141910422-1'
+})
 
 Vue.config.productionTip = false
 
