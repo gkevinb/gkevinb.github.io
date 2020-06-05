4eea185c693b3387dcd677b354a6d292aa709260
Gabor Kevin Barta <gkevinba@gmail.com>
2020-06-05 21:59:02 +0200

    :bug: Close #159 Don't show forked repos under Github Projects section

--- src/components/GithubProjects/GithubProjects.js
+++ src/components/GithubProjects/GithubProjects.js
@@ -2,5 +2,13 @@ export default {
     name: "github-projects-tab",
     props: {
         input: Array
+    },
+    data() {
+        return {
+            repos: []
+        };
+    },
+    created() {
+        this.repos = this.input.filter(repo => repo.fork === false)
     }
 };
